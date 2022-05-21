d831f55898846829cc978b237d34a685bcfc48f5
Brian Rono <brishadow24@gmail.com>
2022-05-21 17:43:19 +0300

    same code haha
    the difference comes in memory allocation

---
+++ Structures.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+    struct{
+    
+    char *course;
+
+    } brian, haron ,austin ,daisy;
+
+
+    int main()
+{
+    brian.course = "Mechanical Engineering";
+    haron.course = "Computer Science";
+    austin.course = "Chemical Engineering";
+    daisy.course = "Software Engineering";
+
+    printf("brian %s\n" ,brian.course); 
+    printf("haron %s \n" ,haron.course); 
+    printf("austin %s\n" ,austin.course);
+    printf("daisy %s\n" ,daisy.course) ; 
+    
+    return 0;
+}
